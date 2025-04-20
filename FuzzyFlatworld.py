6870f345c7c3a681be461292b9269d8bc2dca734
miltiades-the-general <miltiadesgeneral@gmail.com>
2025-04-19 18:03:51 -0700

    Changed structure

--- FuzzyFlatworld.py
+++ FuzzyFlatworld.py
@@ -1,6 +1,5 @@
 from Flatworld import *
 
-# Define the cardinal directions
 N = 0
 S = 1
 E = 2
@@ -12,19 +11,25 @@ class Fuzzy:
 
     def __and__(self, other):
         return Fuzzy(self.value * other.value)
-    
+
     def __invert__(self):
         return Fuzzy(1 - self.value)
-    
+
     def __or__(self, other):
         return ~((~self) & (~other))
-    
+
     def __gt__(self, other):
         return self.value > other.value
-    
+        
     def __lt__(self, other):
         return self.value < other.value
-    
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
 class Spot:
     def __init__(self, position, thing):
         vector = thing.position - position
@@ -34,53 +39,48 @@ class Spot:
 
         if self.bearing < 0:
             self.direction.add(N)
-        
+
         if self.bearing > 0:
             self.direction.add(S)
-        
+
         if abs(self.bearing) < 90:
             self.direction.add(E)
-        
+
         if abs(self.bearing) > 90:
             self.direction.add(W)
-        
-        if isinstance(thing, Grass):
+
+        if isinstance(thing,Grass):
             self.kind = 'food'
             self.isClose = closeTo(self.distance)
-        
-        elif isinstance(thing, Red):
-            self.kind = 'danger'
-            self.isClose = closeTo(self.distance)
 
         elif isinstance(thing, Red):
             self.kind = 'danger'
             self.isClose = closeTo(self.distance)
 
-        elif isinstance(thing, Blue):
+        elif isinstance(thing,Blue):
             self.kind = 'friend'
             self.isClose = closeTo(self.distance)
 
         else:
-            print('Unknown thing', thing)
+            print('Unknown thing:', thing)
 
 def getSpots(me):
-    spots = []
-    for organism in getOrganisms():
+    spots = [ ]
+    for organism in getOrganisms( ):
         if organism != me:
             s = Spot(me.position, organism)
             spots.append(s)
     return spots
 
 def getCloseIn(me):
-    isCloseIn = [{'food': Fuzzy(0), 
-                    'danger': Fuzzy(0),
-                    'friend': Fuzzy(0)} for i in 'NSEW']
-    
+    isCloseIn = [ {'food': Fuzzy(0),
+                   'danger': Fuzzy(0),
+                   'friend' : Fuzzy(0)} for i in 'NSEW' ]
+
     for spot in me.spots:
         for d in range(4):
             if d in spot.direction:
                 isCloseIn[d][spot.kind] |= spot.isClose
-
     return isCloseIn
 
 class FuzzyLogicBot(Blue):
@@ -89,36 +89,42 @@ class FuzzyLogicBot(Blue):
         self.scared = Fuzzy(0)
 
     def calcLogic(self):
-        self.isGoodIn = []
-        self.isBadIn = []
-
+        self.isGoodIn = [ ]
+        self.isBadIn = [ ]
         for isClose in self.isCloseIn:
-            isGood = isClose['food'] & ~isClose['friend'] & ~isClose['danger']
 
+            isGood = isClose['food'] & \
+                        ~isClose['friend'] & \
+                        ~isClose['danger']
             isBad = isClose['danger']
 
             self.isGoodIn.append(isGood)
             self.isBadIn.append(isBad)
-    
+
     def handleScared(self):
-        if self.isBadIn[N] | self.isBadIn[S] | self.isBadIn[E] | self.isBadIn[W] > Fuzzy(0.5):
+        if self.isBadIn[N] |    \
+                self. isBadIn[S] |  \
+                self.isBadIn[E] |   \
+                self.isBadIn[W] > \
+                Fuzzy(0.5):
+
             self.scared = Fuzzy(1)
         else:
             self.scared &= Fuzzy(0.9)
-    
+
     def moveDirection(self):
         self.spots = getSpots(self)
         self.isCloseIn = getCloseIn(self)
-        self.calcLogic()
-        self.handleScared()
+        self.calcLogic( )
+        self.handleScared( )
 
         if self.scared > Fuzzy(0.3):
-            return self.chooseScaredDirection()
+            return self.chooseScaredDirection( )
         else:
-            return self.chooseUnscaredDirection()
-        
+            return self.chooseUnscaredDirection( )
+
     def chooseScaredDirection(self):
-        move  = pygame.Vector2()
+        move = pygame.Vector2()
 
         if self.isBadIn[S] > self.isBadIn[N]:
             move.y = -1
@@ -127,37 +133,38 @@ class FuzzyLogicBot(Blue):
 
         if self.isBadIn[E] > self.isBadIn[W]:
             move.x = -1
-        
         else:
             move.x = 1
         return move
-    
+
     def chooseUnscaredDirection(self):
         move = pygame.Vector2()
 
         if self.isGoodIn[N] > self.isGoodIn[S]:
             move.y = -1
-        else:
+        else:            
             move.y = 1
-        
+
         if self.isGoodIn[W] > self.isGoodIn[E]:
             move.x = -1
         else:
-            move.x = 1
+             move.x = 1
         return move
-    
+
+
+
 def closeTo(dist):
-    return Fuzzy(1 / (max(1, dist)/20)**2)
+    return Fuzzy(1 / (max(1,dist)/20)**2)
 
 if __name__ == '__main__':
     pygame.init()
 
-    ticks = mainLoop(initialSpawns = { Red: 4,
-                                      FuzzyLogicBot: 10,
-                                      Grass: 60},
-                    spawnInterval=30,
-                    periodicSpawns={Grass: 4},
-                    boardSize=(750, 500),
-                    playerType=Blue)
-    print('That game lasted ', ticks, ' ticks')
+    ticks = mainLoop(initialSpawns = {Red: 4,
+                                     FuzzyLogicBot: 10,
+                                     Grass : 60},
+                         spawnInterval=30,
+                         periodicSpawns={Grass: 4},
+                         boardSize=(750,500),
+                         playerType=Blue)
+    print('That game lasted', ticks,'ticks')   
     pygame.quit()
